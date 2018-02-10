912c65bca571c67d513dcefcbfed0091797a4d1f
Quinn <hirt.14@wright.edu>
2018-02-10 16:15:22 -0500

    fixed class cast exception in controller

--- src/main/java/edu/wright/dase/OplaController.java
+++ src/main/java/edu/wright/dase/OplaController.java
@@ -4,21 +4,29 @@ import java.util.Set;
 
 import org.protege.editor.owl.model.OWLModelManager;
 import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLOntology;
 
 public class OplaController
 {
 	private OWLModelManager modelManager;
-
+	private OWLOntology     owlOntology;
+	
 	public OplaController(OWLModelManager modelManager)
 	{
 		this.modelManager = modelManager;
+		this.owlOntology  = modelManager.getActiveOntology();
 	}
-
+	
 	public String[] retireveClasses(OWLModelManager modelManager)
 	{
-		String[] classesArray = this.modelManager.getActiveOntology().getClassesInSignature()
-		        .toArray(new String[this.modelManager.getActiveOntology().getClassesInSignature().toArray().length]);
-		return classesArray;
+		Set<OWLClass> set = this.owlOntology.getClassesInSignature();
+		OWLClass[] owlArr = set.toArray(new OWLClass[set.size()]);
+		String[] stringArr = new String[owlArr.length];
+		for(int i = 0; i < stringArr.length; i++)
+		{
+			stringArr[i] = owlArr[i].toString();
+		}
+		return stringArr;
 	}
 
 	public OWLModelManager getModelManager()
